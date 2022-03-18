405753f6b70bdb0b7cd23e0c7dbd20f41b0a7924
rahk1990 <rahkem.ellis@gmail.com>
2022-03-18 16:42:49 -0400

    added functions for comment app for POST

--- backend/drf_jwt_backend/comment/views.py
+++ backend/drf_jwt_backend/comment/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
+from .serializers import CommentSerializer
+from .models import Comment
+from django.shortcuts import get_object_or_404
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.decorators import api_view, permission_classes
 
+ # handles calling the complete comment list
+
+
+@api_view(['GET'])
+def comment_list(request):
+    comments_param = request.query_params.get('video_id')
+    sort_param = request.query_params.get('sort')
+    comment = Comment.objects.all()
+    if comments_param:
+        comment = comment.filter(video_id=comments_param)
+    elif sort_param:
+        comment = comment.order_by(sort_param)
+    serializer = CommentSerializer(comment, many=True)
+    return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+
+@api_view(['GET', 'POST'])
+@permission_classes([IsAuthenticated])
+def user_comments(request):
+    print('User ', f"{request.user.id} {request.user.email} {request.user.username}")
+    if request.method == "POST":
+        serializer = CommentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 # Create your views here.
+
+
+
--- backend/drf_jwt_backend/reply/views.py
+++ backend/drf_jwt_backend/reply/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
+from .serializers import ReplySerializer
+from .models import Reply
+from django.shortcuts import get_object_or_404
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.decorators import api_view, permission_classes
+
+ # handles calling the complete comment list
+# @api_view(['GET', 'POST'])
+# @permission_classes([IsAuthenticated])
+# def user_reply(request):
+
+    
+    # reply_params = request.query_params.get('user_id')
+    # sort_param = request.query_params.get('sort')
+    # reply = Reply.objects.all()
+    # if reply_params:
+    #     reply = reply.filter(user_id =reply_param)
+    
+    # elif sort_param:
+    #     serializer = ReplySerializer(reply, many=True)
+  
+    #     reply = reply.order_by(sort_param)
+    
+    
+    
+    
+    
+    # if request.method == 'GET':
+
+    #     serializer = SongSerializer(reply, many=True)
+
+    #     return Response(serializer.data)
 
 # Create your views here.
