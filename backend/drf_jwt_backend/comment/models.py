39173c29007369a29a03a38d935321bd5acc8288
unknown <qwhick2@gmail.com>
2022-03-18 12:47:34 -0400

    Created model, serializers, and urls Files + Created Property Names in models.py File

--- backend/drf_jwt_backend/comment/models.py
+++ backend/drf_jwt_backend/comment/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Comment(models.Model):
+    users = models.ForeignKey(on_delete=models.CASCADE)
+    video_id = models.CharField(max_length=255)
+    text = models.CharField(max_length=255)
+    likes = models.IntegerField()
+    dislikes = models.IntegerField()
